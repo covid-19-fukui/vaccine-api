6485b1ca312abf6b762ee2f2cebc299fe93edf75
FooQoo <horseshoes.buzz.17@gmail.com>
2021-06-19 20:11:02 +0900

    リファクタリング

--- functions/src/service/vaccine.service.ts
+++ functions/src/service/vaccine.service.ts
@@ -5,7 +5,7 @@ import VaccineEntity from '../repository/dto/vaccine.entity';
 import VaccineListApiResponse from '../controller/dto/vaccine.list.api.response';
 import { VaccineFireStoreRepository } from '../repository/vaccine.repository';
 import * as moment from 'moment-timezone';
-import VaccineApiResponse from 'src/controller/dto/vaccine.api.response';
+import VaccineApiResponse from '../controller/dto/vaccine.api.response';
 import VaccineQuery from '../controller/dto/vaccine.query';
 import * as functions from 'firebase-functions';
 
@@ -26,76 +26,11 @@ export class VaccineService {
   ) {}
 
   /**
-   * ワクチン接種状況の一覧取得
-   *
-   * @param {string} prefectureCode 都道府県コード
-   * @returns {Promise<VaccineListApiResponse>} ワクチン接種状況
-   */
-  async findVaccines(
-    vaccineQuery: VaccineQuery,
-  ): Promise<VaccineListApiResponse> {
-    const now = this.getNow();
-
-    log.info(vaccineQuery);
-
-    const vaccineEntity: VaccineEntity[] = await this.vaccineFireStoreRepository.getVaccines(
-      vaccineQuery.prefectureCode,
-      vaccineQuery.count,
-    );
-
-    const vaccineResponse: VaccineResponse[] = vaccineEntity.map((entity) =>
-      this.buildVaccineResponse(entity),
-    );
-
-    return new VaccineListApiResponse(
-      new InfoResponse(now, vaccineQuery.prefectureCode),
-      vaccineResponse,
-    );
-  }
-
-  /**
-   * ワクチン接種状況の取得
-   *
-   * @param {string} prefectureCode 都道府県コード
-   * @param {string} date 集計対象日
-   * @returns {Promise<VaccineListApiResponse>} ワクチン接種状況
-   */
-  async getVaccine(
-    prefectureCodeString: string,
-    dateString: string,
-  ): Promise<VaccineApiResponse> {
-    const now = this.getNow();
-
-    const prefectureCode = this.convertStringToNum(prefectureCodeString);
-    const date = this.convertStringToDate(dateString);
-
-    const vaccineEntity: VaccineEntity[] = await this.vaccineFireStoreRepository.getVaccine(
-      prefectureCode,
-      date,
-    );
-
-    if (vaccineEntity.length <= 0) {
-      throw new NotFoundException(
-        `Requested vaccination data does not exist: {prefectureCode: ${prefectureCode}, date: {dateString}}`,
-      );
-    }
-
-    const vaccineResponse: VaccineResponse = vaccineEntity.map((entity) =>
-      this.buildVaccineResponse(entity),
-    )[0];
-
-    return new VaccineApiResponse(
-      new InfoResponse(now, prefectureCode),
-      vaccineResponse,
-    );
-  }
-
-  /**
    * 現在時刻を取得する
    *
    * @returns {string} 文字列化した現在時刻
    */
-  private getNow(): string {
+  private static getNow(): string {
     return moment().tz('Asia/Tokyo').format();
   }
 
@@ -105,7 +40,7 @@ export class VaccineService {
    * @param {string} prefectureCode
    * @returns {number} 数値型の都道府県コード
    */
-  private convertStringToNum(prefectureCode: string): number {
+  private static convertStringToNum(prefectureCode: string): number {
     return Number(prefectureCode);
   }
 
@@ -114,7 +49,7 @@ export class VaccineService {
    * @param date
    * @returns
    */
-  private convertStringToDate(date: string): moment.Moment {
+  private static convertStringToDate(date: string): moment.Moment {
     return moment.tz(date, 'Asia/Tokyo');
   }
 
@@ -124,8 +59,10 @@ export class VaccineService {
    * @param {VaccineEntity} vaccineEntity firebaseから取得したワクチン接種状況
    * @returns {VaccineResponse} レスポンスクラス
    */
-  private buildVaccineResponse(vaccineEntity: VaccineEntity): VaccineResponse {
-    const date = this.formatDate(vaccineEntity.date.toDate());
+  private static buildVaccineResponse(
+    vaccineEntity: VaccineEntity,
+  ): VaccineResponse {
+    const date = VaccineService.formatDate(vaccineEntity.date.toDate());
 
     return new VaccineResponse(
       date,
@@ -158,10 +95,77 @@ export class VaccineService {
    * @param {Date} date Date
    * @returns {string} YYYY-MM-DD形式の日付
    */
-  private formatDate(date: Date): string {
+  private static formatDate(date: Date): string {
     const year = date.getFullYear();
     const month = ('00' + (date.getMonth() + 1)).slice(-2);
     const day = ('00' + date.getDate()).slice(-2);
     return year + '-' + month + '-' + day;
+  }
+
+  /**
+   * ワクチン接種状況の一覧取得
+   *
+   * @param {VaccineQuery} vaccineQuery クエリパラメータ
+   * @returns {Promise<VaccineListApiResponse>} ワクチン接種状況
+   */
+  async findVaccines(
+    vaccineQuery: VaccineQuery,
+  ): Promise<VaccineListApiResponse> {
+    const now = VaccineService.getNow();
+
+    log.info(vaccineQuery);
+
+    const vaccineEntity: VaccineEntity[] = await this.vaccineFireStoreRepository.getVaccines(
+      vaccineQuery.prefectureCode,
+      vaccineQuery.count,
+    );
+
+    const vaccineResponse: VaccineResponse[] = vaccineEntity.map((entity) =>
+      VaccineService.buildVaccineResponse(entity),
+    );
+
+    return new VaccineListApiResponse(
+      new InfoResponse(now, vaccineQuery.prefectureCode),
+      vaccineResponse,
+    );
+  }
+
+  /**
+   * ワクチン接種状況の取得
+   *
+   * @param {string} prefectureCodeString 都道府県コード
+   * @param {string} dateString 集計対象日
+   * @returns {Promise<VaccineListApiResponse>} ワクチン接種状況
+   */
+  async getVaccine(
+    prefectureCodeString: string,
+    dateString: string,
+  ): Promise<VaccineApiResponse> {
+    const now = VaccineService.getNow();
+
+    const prefectureCode = VaccineService.convertStringToNum(
+      prefectureCodeString,
+    );
+    const date = VaccineService.convertStringToDate(dateString);
+
+    const vaccineEntity: VaccineEntity[] = await this.vaccineFireStoreRepository.getVaccine(
+      prefectureCode,
+      date,
+    );
+
+    if (vaccineEntity.length <= 0) {
+      throw new NotFoundException(
+        `Requested vaccination data does not exist: {prefectureCode: ${prefectureCode}, date: {dateString}}`,
+      );
+    }
+
+    const vaccineResponse: VaccineResponse = vaccineEntity.map((entity) =>
+      VaccineService.buildVaccineResponse(entity),
+    )[0];
+
+    return new VaccineApiResponse(
+      new InfoResponse(now, prefectureCode),
+      vaccineResponse,
+    );
   }
 }
